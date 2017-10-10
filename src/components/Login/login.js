a003a94b3977c4986ca1c8e926cf51f9bef7d82b
Maria Stommes <mstommes@gmail.com>
2017-10-09 20:51:28 -0500

    make it look a bit prettier

--- src/components/Login/login.js
+++ src/components/Login/login.js
@@ -1,17 +1,28 @@
 import React from 'react';
+import styled from 'styled-components'
 import './style.css';
+
+const FormStyle = styled.div`
+ border-color: black;
+ border-style: solid;
+ border-width: 2px;
+ margin:auto;
+ margin-top: 5px;
+ width: 300px;
+ padding: 25px;
+ text-align: left;
+`;
 
 class Login extends React.Component {
   render() {
     return (
-      <div className="login-box">
-          <h2>Login:</h2>
+        <FormStyle> 
           <form>
-          <label>Name: <input type="text" name="name" placeholder="User Name" /></label>
-          <label>Password: <input type="text" name="name" placeholder="***********"/></label>
-          <input type="submit" value="Submit" />
-        </form>
-      </div>
+            <label>Name: <input type="text" name="name" placeholder="User Name" /></label><br />
+            <label>Password: <input type="text" name="name" placeholder="***********"/></label><br />
+            <input type="submit" value="Submit" />
+          </form>
+        </FormStyle>
     );
   }
 }
